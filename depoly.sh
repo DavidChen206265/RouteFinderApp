87d6af0a2f38a01faaf5d0c1e8abf19746427aa5
DavidChen206265 <davidchen206265@gmail.com>
2026-03-17 19:30:54 +0000

    Deploying to gh-pages from @ DavidChen206265/RouteFinderApp@848d82f942841a94f5c67ca987ede509c4d1e8dd 🚀

---
+++ depoly.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# RouteFinderApp Deploy Script
+
+cd ~/projects/RouteFinderApp || exit
+
+git pull
+
+sudo docker compose up -d --force-recreate
+
+sudo docker image prune -f
+
+echo "Depolyment completed."
